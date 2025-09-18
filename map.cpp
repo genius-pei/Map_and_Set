705ddffa2faf83c3d9b1dbf3d271b7b6c84d1bbc
Yiming <2018565194@qq.com>
2025-09-18 23:11:32 +0800

    Update map.cpp

--- map.cpp
+++ map.cpp
@@ -4,23 +4,51 @@
 using namespace std;
 void test01()
 {
-	pair<string, string> kv1("apple", "苹果");
-	pair<string, string> kv2("banana", "香蕉");
-	pair<string, string> kv3("orange", "橘子");
-	map<string, string> dicrtory = {kv1,kv2,kv3};
-	dicrtory.insert(make_pair("money", "软妹币"));
-	auto it = dicrtory.begin();
-	while (it != dicrtory.end())
+//	pair<string, string> kv1("apple", "苹果");
+//	pair<string, string> kv2("banana", "香蕉");
+//	pair<string, string> kv3("orange", "橘子");
+//	map<string, string> dicrtory = {kv1,kv2,kv3};
+//	dicrtory.insert(make_pair("money", "软妹币"));
+//	auto it = dicrtory.begin();
+//	while (it != dicrtory.end())
+//	{
+//		//it->second = "xyz";
+//		cout << (*it).first << ":" << (*it).second << endl;
+//		++it;
+//	}
+//	cout << endl; 
+//	for (auto& e : dicrtory)
+//	{
+//		cout << e.first << ":" << e.second << endl;
+//	}
+//	//结构化绑定
+//	
+//
+//	auto& [x1, y1] = kv1;
+	string arr[] = { "苹果", "西瓜", "苹果", "西瓜", "苹果", "苹果", "西瓜","苹果", "香蕉", "苹果", "香蕉" };
+	map<string, int> countMap;
+	for (auto& str : arr)
 	{
-		//it->second = "xyz";
-		cout << (*it).first << ":" << (*it).second << endl;
-		++it;
+		//map<string, int>::iterator it = countMap.find(str);
+		auto it = countMap.find(str);
+		if(it != countMap.end())//已经存在
+		{
+			it->second++;
+		}
+		else
+		{
+			countMap.insert({str, 1});
+		}
 	}
-	cout << endl;
-	for (auto& e : dicrtory)
+	//for (auto& e : countMap)
+	//{
+	//	cout << e.first << ":" << e.second << endl;
+	//}
+	for (auto& [k, v] : countMap)
 	{
-		cout << e.first << ":" << e.second << endl;
+		cout << k << ":" << v << endl;
 	}
+
 
 }
 int main()
